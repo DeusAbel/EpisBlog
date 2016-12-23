ad820100cde544f8d113b2f3424e2ac10531915e
ABEL-PC\Abel <abel.harc@live.com>
2016-12-22 23:48:00 -0500

    Modificacion de atributos en el modelo

--- app_api/models/esquemas.js
+++ app_api/models/esquemas.js
@@ -11,14 +11,16 @@ var entradaSchema = new Schema({
   autor_id:           {type: Schema.ObjectId, ref: 'Usuario'}, 
   titulo:             {type: String,  required: true},   
   descripcion:        {type: String,  required: true},
-  fecha_creacion:     {type: Date,    required: true}
+  fecha_creacion:     {type: Date,    required: true},
+  fecha_modificacion: {type: Date,    required: true}
 });
 
 var comentarioSchema = new Schema({    
-  autor_id:     {type: Schema.ObjectId, ref: 'Usuario'}, 
-  entrada_id:   {type: Schema.ObjectId, ref: 'Entrada'}, 
-  comentario:   {type: String,  required: true},  
-  fecha:        {type: Date,    required: true},    
+  autor_id:           {type: Schema.ObjectId, ref: 'Usuario'}, 
+  entrada_id:         {type: Schema.ObjectId, ref: 'Entrada'}, 
+  comentario:         {type: String,  required: true},  
+  fecha_creacion:     {type: Date,    required: true},
+  fecha_modificacion: {type: Date,    required: true} 
 });
 
 var usuario       = mongoose.model('Usuario', usuarioSchema);
